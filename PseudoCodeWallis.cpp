3a29e63c98d146c39517ceeeb9e7764bb45a2643
HappiLesss <max.schmi21@gmail.com>
2026-09-18 13:32:14 +0200

    Update PseudoCodeWallis.cpp
    Ajout du pseudo code

--- PseudoCodeWallis.cpp
+++ PseudoCodeWallis.cpp
@@ -1,1 +1,17 @@
 
+1 : Demander la saisie de l'utilisateur
+1.1 Demander la saisie du numérateur
+1.2 Demander la saisie du dénominateur
+2: Vérifier la saisie
+2.1.1 : Vérifier que le denominateur est différent de 0
+2.1.2 : Vérifier que la différence entre numerateur et denominateur = |1|
+3 :Calculer la suite de Whalis
+3.1 Initialisation du premier terme avec a =2 (numérateur) et b =1 (denominateur)
+3.2 Boucle jusqua que a == numérateur de l'utilisateur et que b == denominateur de l'utilisateur
+3.2.1 stock notre valeure a avant incrémentation dans une variable c
+3.2.2 stock notre valeur b avant incrémentation dans une variable d
+3.2.3 si a > b alors on incrément 2 à b
+3.2.4 si b > a alors on incrément 2 à a
+3.2.5 Calcul de la valeur v = (c*a)/b*d)
+4 : Afficher le résultat
+4.1 Affichage de la variable v dans la console
